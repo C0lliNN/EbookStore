0a07e461fdc0e954d2b10da8b81ab867cf6c73f3
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2021-09-12 13:39:23 -0300

    Add validation

--- internal/auth/delivery/dto/requests.go
+++ internal/auth/delivery/dto/requests.go
@@ -3,11 +3,11 @@ package dto
 import "github.com/c0llinn/ebook-store/internal/auth/model"
 
 type RegisterRequest struct {
-	FirstName            string `json:"firstName"`
-	LastName             string `json:"lastName"`
-	Email                string `json:"email"`
-	Password             string `json:"password"`
-	PasswordConfirmation string `json:"passwordConfirmation"`
+	FirstName            string `json:"firstName" binding:"required,max=150"`
+	LastName             string `json:"lastName" binding:"required,max=150"`
+	Email                string `json:"email" binding:"required,email"`
+	Password             string `json:"password" binding:"required,min=6,max=20"`
+	PasswordConfirmation string `json:"passwordConfirmation" binding:"required,eqfield=Password"`
 }
 
 func (r RegisterRequest) ToDomain(id string) model.User {
